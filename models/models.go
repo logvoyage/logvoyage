9f7bc5bd2a20b6ba14f79be5e5eec5190417dd83
firstrow <firstrow@gmail.com>
2017-06-30 10:38:07 +0300

    Added todo

--- models/models.go
+++ models/models.go
@@ -47,5 +47,5 @@ type BaseModel struct {
 	ID        uint       `gorm:"primary_key" json:"id"`
 	CreatedAt time.Time  `json:"created_at"`
 	UpdatedAt time.Time  `json:"updated_at"`
-	DeletedAt *time.Time `sql:"index" json:"deleted_at"`
+	DeletedAt *time.Time `sql:"index" json:"deleted_at"` // TODO: Remove deleted_at. Records should be deleted permanently.
 }
